d8ff6e7ee8cd1cfdb174e7de518ab1a9166eb22b
Francisco Martin <franmartin241001@gmail.com>
2022-11-24 17:09:47 -0300

    details styled

--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -1,11 +1,15 @@
 import React, { useState, useEffect } from 'react'
 import "./detail.css"
 import {db} from "../components/functions/firebase"
-import {doc, getDoc} from "firebase/firestore"
+import {doc, getDoc, getDocs,collection} from "firebase/firestore"
 import { useParams } from 'react-router-dom'
+import { Spinner } from '../components'
 function Detail() {
   const [details,setDetails] = useState()
+  const [src,setSrc] = useState()
+  const [Array,setArray] = useState([]);
   let {id} = useParams();
+
   
   useEffect(()=>{
     async function data(id){
@@ -16,24 +20,51 @@ function Detail() {
       } else {
         console.log("No such document!");
       }
-    }   
-    data(id) ; 
+    }  
+    data(id);
   },[]);
-  useEffect(()=>{console.log(details)},[details])
 
+  useEffect(()=>{
+    const LogosCollection = collection(db,"logos");
+    getDocs(LogosCollection).then(
+      document  => { setArray(document.docs[1].data().logos.urls); Array.find((url)=>{
+        id===url.id && setSrc(url.url)
+      })}
+    ); 
+  },[details])
+
+  const modelRef = React.useRef();
   return(
     <div className='detail_cont'>
       {details !== undefined ? (
         <>
+        <model-viewer
+      class="model-detail"
+      src={src}
+      alt="A rock"
+      skybox
+      exposure="0.9"
+      camera-controls
+      ar-scale="auto"
+      ar-modes="webxr"
+      camera-orbit="90deg 90deg 4.7m"
+      max-camera-orbit="auto auto 6m"
+      ref={(ref) => {
+        modelRef.current = ref;
+      }}
+      >
+    </model-viewer>
+    <div className='detail_text'>
         <p>Familia:{details.Familia}</p>
         <p>Pueblo de Origen: {details.PuebloOrigen}</p>
         <p>Tomo: {details.Tomos}</p>
         <br />
-        <p>Texto sin Traducir: {details.TextoSinTraducir}</p>
+        <p className='detail_texto'>Texto sin Traducir: {details.TextoSinTraducir}</p>
         <br />
-        <p>Texto Traducido: {details.TextoTraducido}</p>
+        <p className='detail_texto'>Texto Traducido: {details.TextoTraducido}</p>
+    </div>
         </>
-      ):("no hay datos")
+      ):(<><Spinner/></>)
       }
     </div>
   )
